e2fe7dd143f671dc03b69cb80f8e3ba39cb110c6
Filipe Fiusa <flafigol@gmail.com>
2022-03-30 21:42:49 -0300

    Fixed a bug where the chapters to read on library dont show

---
+++ app/src/main/java/com/example/mobileproject/ui/library/NovelListDiffCallback.java
@@ -0,0 +1,50 @@
+package com.example.mobileproject.ui.library;
+
+import androidx.annotation.Nullable;
+import androidx.recyclerview.widget.DiffUtil;
+
+import com.example.mobileproject.model.NovelDetails;
+
+import java.util.List;
+
+public class NovelListDiffCallback extends DiffUtil.Callback {
+
+    private final List<NovelDetails> mOldNovelDetailsList;
+    private final List<NovelDetails> mNewNovelDetailsList;
+
+    public NovelListDiffCallback(List<NovelDetails> mOldNovelDetailsList, List<NovelDetails> mNewNovelDetailsList) {
+        this.mOldNovelDetailsList = mOldNovelDetailsList;
+        this.mNewNovelDetailsList = mNewNovelDetailsList;
+    }
+
+    @Override
+    public int getOldListSize() {
+        return mOldNovelDetailsList.size();
+    }
+
+    @Override
+    public int getNewListSize() {
+        return mNewNovelDetailsList.size();
+    }
+
+    @Override
+    public boolean areItemsTheSame(int oldItemPosition, int newItemPosition) {
+        return mOldNovelDetailsList.get(oldItemPosition).getDb_id() == mNewNovelDetailsList.get(
+                newItemPosition).getDb_id();
+    }
+
+    @Override
+    public boolean areContentsTheSame(int oldItemPosition, int newItemPosition) {
+        final NovelDetails oldEmployee = mOldNovelDetailsList.get(oldItemPosition);
+        final NovelDetails newEmployee = mNewNovelDetailsList.get(newItemPosition);
+
+        return oldEmployee.getChapterToReadQuantity() == newEmployee.getChapterToReadQuantity();
+    }
+
+    @Nullable
+    @Override
+    public Object getChangePayload(int oldItemPosition, int newItemPosition) {
+        // Implement method if you're going to use ItemAnimator
+        return super.getChangePayload(oldItemPosition, newItemPosition);
+    }
+}
--- app/src/main/java/com/example/mobileproject/ui/library/NovelsAdapter.java
+++ app/src/main/java/com/example/mobileproject/ui/library/NovelsAdapter.java
@@ -14,6 +14,7 @@ import android.widget.TextView;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.content.ContextCompat;
+import androidx.recyclerview.widget.DiffUtil;
 import androidx.recyclerview.widget.RecyclerView;
 import androidx.swiperefreshlayout.widget.SwipeRefreshLayout;
 
@@ -58,10 +59,12 @@ public class NovelsAdapter extends RecyclerView.Adapter<NovelsAdapter.NovelDetai
     }
 
     public void updateNovelsList(ArrayList<NovelDetails> chapterList){
-        this.mNovelList = new ArrayList<>();
+        final NovelListDiffCallback diffCallback = new NovelListDiffCallback(this.mNovelList, chapterList);
+        final DiffUtil.DiffResult diffResult = DiffUtil.calculateDiff(diffCallback);
 
-        this.mNovelList = chapterList;
-        notifyDataSetChanged();
+        this.mNovelList.clear();
+        this.mNovelList.addAll(chapterList);
+        diffResult.dispatchUpdatesTo(this);
     }
 
     public void resetSelectedList(){
